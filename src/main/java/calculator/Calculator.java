92c41cccb4c8b61f11e3a36c8cd984ef155e64c4
TheDeveloperKwon <akongwon@gmail.com>
2026-03-19 19:44:23 +0900

    feat: 사칙 연산 계산기 구현 1단계

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,20 @@
+package calculator;
+
+public class Calculator {
+    int add(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    int substract(int num1, int num2) {
+    return num1 - num2;
+    }
+
+    int multiply(int num1, int num2){
+        return num1 * num2;
+    }
+
+    int divide(int num1, int num2){
+        return num1 / num2;
+    }
+
+}
