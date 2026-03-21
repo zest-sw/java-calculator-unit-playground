15a98468395c692005525f4a24fe23032776793b
TheDeveloperKwon <akongwon@gmail.com>
2026-03-21 14:48:24 +0900

    refactor: 문자열 계산기 assertj로 리팩터링

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -1,6 +1,7 @@
 package calculator;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
@@ -16,7 +17,8 @@ class CalculatorTest {
         int actual = calculator.add(3,6);
 
         // then
-        assertEquals(9,actual);
+        //assertEquals(9,actual);
+        assertThat(actual).isEqualTo(9);
     }
 
     @DisplayName("두 개의 정수를 뺀 결과를 반환할 수 있다.")
@@ -29,7 +31,8 @@ class CalculatorTest {
         int actual = calculator.substract(3,6);
 
         // then
-        assertEquals(-3, actual);
+        //assertEquals(-3, actual);
+        assertThat(actual).isEqualTo(-3);
     }
 
     @DisplayName("두 개의 정수를 곱한 결과를 반환할 수 있다.")
@@ -42,7 +45,8 @@ class CalculatorTest {
         int actual = calculator.multiply(3,6);
 
         // then
-        assertEquals(18,actual);
+        //assertEquals(18,actual);
+        assertThat(actual).isEqualTo(18);
     }
 
     @DisplayName("두 개의 정수를 나눈 결과를 반환할 수 있다.")
@@ -55,6 +59,77 @@ class CalculatorTest {
         int actual = calculator.divide(3,6);
 
         // then
-        assertEquals(0,actual);
+        //assertEquals(0,actual);
+        assertThat(actual).isEqualTo(0);
+    }
+
+    @DisplayName("\"\" => 0")
+    @Test
+    void testCalStr1() {
+        // given
+        Calculator calculator = new Calculator();
+
+        // when
+        int actual = calculator.calculate_str("");
+
+        // then
+        //assertEquals(0,actual);
+        assertThat(actual).isEqualTo(0);
+    }
+
+    @DisplayName("\"1,2\" => 3")
+    @Test
+    void testCalStr2() {
+        // given
+        Calculator calculator = new Calculator();
+
+        // when
+        int actual = calculator.calculate_str("1,2");
+
+        // then
+        //assertEquals(3,actual);
+        assertThat(actual).isEqualTo(3);
+    }
+
+    @DisplayName("\"1,2,3\" => 6")
+    @Test
+    void testCalStr3() {
+        // given
+        Calculator calculator = new Calculator();
+
+        // when
+        int actual = calculator.calculate_str("1,2,3");
+
+        // then
+        //assertEquals(6,actual);
+        assertThat(actual).isEqualTo(6);
+    }
+
+    @DisplayName("\"1,2:3\" => 6")
+    @Test
+    void testCalStr4() {
+        // given
+        Calculator calculator = new Calculator();
+
+        // when
+        int actual = calculator.calculate_str("1,2:3");
+
+        // then
+        //assertEquals(6,actual);
+        assertThat(actual).isEqualTo(6);
+    }
+
+    @DisplayName("//;\\n1;2;3")
+    @Test
+    void testCalStr5() {
+        // given
+        Calculator calculator = new Calculator();
+
+        // when
+        int actual = calculator.calculate_str("//;\\n1;2;3");
+
+        // then
+        //assertEquals(6,actual);
+        assertThat(actual).isEqualTo(6);
     }
 }
