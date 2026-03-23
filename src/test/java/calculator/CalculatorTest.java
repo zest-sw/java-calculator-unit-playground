c213f3614af915ffc7a00af998cd55de267a1299
TheDeveloperKwon <akongwon@gmail.com>
2026-03-23 14:24:54 +0900

    refactor: ParameterizedTest 사용으로 반복 코드 제거

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -3,6 +3,8 @@ package calculator;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
 
@@ -23,12 +25,12 @@ class CalculatorTest {
 
     @DisplayName("두 개의 정수를 뺀 결과를 반환할 수 있다.")
     @Test
-    void testSubstract() {
+    void testSubtract() {
         // given
         Calculator calculator = new Calculator();
 
         // when
-        int actual = calculator.substract(3,6);
+        int actual = calculator.subtract(3,6);
 
         // then
         //assertEquals(-3, actual);
@@ -63,73 +65,20 @@ class CalculatorTest {
         assertThat(actual).isEqualTo(0);
     }
 
-    @DisplayName("\"\" => 0")
-    @Test
-    void testCalStr1() {
-        // given
+    @ParameterizedTest
+    @CsvSource({
+            "'1,2,3', 6",
+            "'1:2:3', 6",
+            "'//;\\n1;2;3', 6",
+            "'', 0",
+            "'7', 7"
+    })
+    void test_calculate_str(String input, int expected) {
+        // Given & When
         Calculator calculator = new Calculator();
+        int result = calculator.calculate_str(input);
 
-        // when
-        int actual = calculator.calculate_str("");
-
-        // then
-        //assertEquals(0,actual);
-        assertThat(actual).isEqualTo(0);
-    }
-
-    @DisplayName("\"1,2\" => 3")
-    @Test
-    void testCalStr2() {
-        // given
-        Calculator calculator = new Calculator();
-
-        // when
-        int actual = calculator.calculate_str("1,2");
-
-        // then
-        //assertEquals(3,actual);
-        assertThat(actual).isEqualTo(3);
-    }
-
-    @DisplayName("\"1,2,3\" => 6")
-    @Test
-    void testCalStr3() {
-        // given
-        Calculator calculator = new Calculator();
-
-        // when
-        int actual = calculator.calculate_str("1,2,3");
-
-        // then
-        //assertEquals(6,actual);
-        assertThat(actual).isEqualTo(6);
-    }
-
-    @DisplayName("\"1,2:3\" => 6")
-    @Test
-    void testCalStr4() {
-        // given
-        Calculator calculator = new Calculator();
-
-        // when
-        int actual = calculator.calculate_str("1,2:3");
-
-        // then
-        //assertEquals(6,actual);
-        assertThat(actual).isEqualTo(6);
-    }
-
-    @DisplayName("//;\\n1;2;3")
-    @Test
-    void testCalStr5() {
-        // given
-        Calculator calculator = new Calculator();
-
-        // when
-        int actual = calculator.calculate_str("//;\\n1;2;3");
-
-        // then
-        //assertEquals(6,actual);
-        assertThat(actual).isEqualTo(6);
+        // Then
+        assertThat(result).isEqualTo(expected);
     }
 }
